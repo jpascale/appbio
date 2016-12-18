27d6f83fc49e455c7e699a5289a7662bf2866fc7
ludvigla <ludvigla@kth.se>
2016-12-19 00:35:46 +0100

    Added argparse module with a few optional arguments like threshold value, window size, window percentage and minimum read length. We should figure out what type of statistics we want to have as well. This version will print to an output file if -out 'filename' is specified and to stdout if no output name is given. You can ofc just direct the output from stdout to a new file using '>' in the terminal but I think it's still a nice feature to have.

--- trim.py
+++ trim.py
@@ -1,13 +1,66 @@
 #!/usr/bin/env python2
 from Bio import SeqIO
 import sys
-import time
+#import time
+import argparse
 
-THRESHOLD = 20              #Quality threshold
-WINDOW_PERCENT = 0.05       #Used to calculate window length from total length
-WINDOW_DEFAULT = 3          #Value used for window length as a minimum
-READ_LENGTH_DEFAULT = 20    #Minumum read length
-
+def setting_variables():
+    """
+    Define parameters from user settings.
+    """
+    parser = argparse.ArgumentParser()
+    #Positional argument
+    parser.add_argument("input", 
+                        help="Give path to input file. Only fastq format allowed.")    
+    #Optional arguments
+    parser.add_argument("-t", "--threshold", type=int,
+                        help="Set quality threshold for trimming.",
+                        action="store")
+    parser.add_argument("-l", "--length", type=int,
+                        help="Set minimum read length. Any reads smaller than this value will be discarded.",
+                        action="store")
+    parser.add_argument("-w", "--window", type=int,
+                        help="Set window width, i.e. the number of bases to include in each window.",
+                        action="store")
+    parser.add_argument("-p", "--percentage_window", type=float,
+                        help="""Set window size as percentage of total read instead of fixed number of base pairs. 
+                        Minimum window width is set to 3 base pairs. If a default window size is given with -w, 
+                        the largest window size will be selected.""")
+    parser.add_argument("-out", "--outputname", type=str,
+                        help="output file name",
+                        action="store")
+    args = parser.parse_args()
+    
+    global filename
+    filename = args.input
+    
+    global THRESHOLD   
+    if args.threshold:
+        THRESHOLD = args.threshold
+    else:
+        THRESHOLD = 20
+        
+    global READ_LENGTH_DEFAULT
+    if args.length:
+        READ_LENGTH_DEFAULT = args.length
+    else:
+        READ_LENGTH_DEFAULT = 20
+        
+    global WINDOW_DEFAULT
+    if args.window:
+        WINDOW_DEFAULT = args.window
+    else:
+        WINDOW_DEFAULT = 3
+        
+    global WINDOW_PERCENT
+    if args.percentage_window:
+        WINDOW_PERCENT = args.percentage_window
+    else:
+        WINDOW_PERCENT = 0.0 #Default percentage set to 0 so that window width provded by user will be constant
+    
+    if args.outputname:
+        global outputfile
+        outputfile = args.outputname
 
 #TODO: Test
 def window_algorithm(record):
@@ -20,11 +73,10 @@ def window_algorithm(record):
     """
     seq = record.seq
     qual = record.letter_annotations["phred_quality"]
-    print(record.__hash__)
     
     window_len = max(WINDOW_DEFAULT, int(round(len(seq) * WINDOW_PERCENT)))
     sub_rec = 0
-    rem_rec = 0
+    pos = 0
     i = 0
     
     while i + window_len <= len(seq):
@@ -37,15 +89,13 @@ def window_algorithm(record):
             i += 1
         else:
             if i < READ_LENGTH_DEFAULT:
-                sub_rec = 1
                 break
             else:
-                sub_rec = record[0:i]
-                rem_rec = record[i:]
+                sub_rec = 1
+                pos = i
                 break
-
    
-    return sub_rec, rem_rec#, discarded_reads, removed_reads
+    return sub_rec, pos
 
 def get_window_mean(qual):
     """
@@ -54,25 +104,29 @@ def get_window_mean(qual):
     return float(sum(qual))/len(qual)
 
 def main():
-    start = time.time()
+    #start = time.time()
     """
     Parses trhough fastq file and trims each record using the sliding window algorithm.
     """
-    filename = sys.argv[1]
-    output = sys.argv[2]
-    with open(filename) as ih, open(output, 'w') as oh, open('discarded', 'w') as df, open('removed', 'w') as rf:
-        for record in SeqIO.parse(ih, 'fastq'):
-            trimmed_seq, removed = window_algorithm(record)
-
-            if type(trimmed_seq) == type(record):
-                oh.write(trimmed_seq.format('fastq'))
-                rf.write(removed.format('fastq'))
-            elif trimmed_seq == 1:
-                df.write(record.format('fastq'))
-            elif trimmed_seq == 2:
-                oh.write(record.format('fastq'))
+    setting_variables()
+    try:
+        with open(filename) as ih, open(outputfile, 'w') as oh:
+            for record in SeqIO.parse(ih, 'fastq'):
+                trimmed_seq, pos = window_algorithm(record)
+                if trimmed_seq == 1:
+                    oh.write(record[0:pos].format('fastq'))
+                elif trimmed_seq == 2:
+                    oh.write(record.format('fastq'))
+    except NameError:
+        with open(filename) as ih:
+            for record in SeqIO.parse(ih, 'fastq'):
+                trimmed_seq, pos = window_algorithm(record)
+                if trimmed_seq == 1:
+                    sys.stdout.write(record[0:pos].format('fastq'))
+                elif trimmed_seq == 2:
+                    sys.stdout.write(record.format('fastq'))
             
-    end = time.time()
-    print(end - start)
+    #end = time.time()
+    #print(end - start)
 if __name__ == "__main__":
 	main()
